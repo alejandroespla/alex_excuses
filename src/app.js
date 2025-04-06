d93ce17611ba65cc94081de5d6c3237bbbc4a207
Espla <37451532+alejandroespla@users.noreply.github.com>
2025-04-06 19:46:54 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-import "bootstrap";
-import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = function excusaRandom() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let excuseElement = document.querySelector('#excuse'); 
+
+  let who = ['Un gnome', 'Un chupacabras', 'El bigfoot', 'El monstruo del lago Ness', 'Un unicornio'];  
+  let action = ['me robó', 'arrasó', 'volatirizó', 'se comió', 'quemó', 'asaltó'];  
+  let what = ['mi despertador', 'mi coche', 'el autobús', 'el metro entero', 'la ciudad'];  
+  let when = ['a lo largo de la mañana', 'en un abrir y cerrar de ojos', 'anoche mientras dormía', 'mientras desayunaba', 'justo cuando iba a salir'];  
+  
+  let whoIndex = Math.floor(Math.random() * who.length);  
+  let actionIndex = Math.floor(Math.random() * action.length);  
+  let whatIndex = Math.floor(Math.random() * what.length);  
+  let whenIndex = Math.floor(Math.random() * when.length);  
+  
+  let excuse = `${who[whoIndex]} ${action[actionIndex]} ${what[whatIndex]} ${when[whenIndex]}`;  
+  excuseElement.textContent = excuse;
 };
+
+  
+  
